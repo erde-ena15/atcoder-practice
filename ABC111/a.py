5194498e5f549ec3ad62de893c6c5e8606d3609a
erde-ena15 <erde.ena15@gmail.com>
2025-12-19 23:58:01 +0900

    12/19

---
+++ ABC111/a.py
@@ -0,0 +1,16 @@
+def main():
+    num = input()
+    ans = ""
+    for i in range(3):
+        if num[i] == "1":
+            ans += "9"
+        else:
+            ans += "1"
+    print(int(ans))
+main()
+
+#最適解
+def gpt_main():
+    return
+
+#覚えること
